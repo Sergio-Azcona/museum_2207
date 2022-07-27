cbfc935a2b3d85e281f646990d61b36f0961409c
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-07-27 12:02:55 -0400

    Test: all Exhibit class tests set up

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,18 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  let(:exhibit) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+
+  it 'should have a class' do
+    expect(exhibit).to be_a Exhibit
+  end
+
+  it 'should have a name'do
+    expect(exhibit.name).to eq ("Gems and Minerals")
+  end
+
+  it 'should inform the cost' do
+    expect(exhibit.cost).to eq 0
+  end
+end
