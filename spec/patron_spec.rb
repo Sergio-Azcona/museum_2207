48a78bdba58a75139ffbde4c74ee2040d3f5582f
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-07-27 12:21:58 -0400

    Feat: Patron class test and feat complete

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -4,19 +4,19 @@ require './lib/patron'
 RSpec.describe Patron do
   let(:patron_1) { Patron.new("Bob", 20) }
 
-  xit 'should have a class' do
+  it 'should have a class' do
     expect(patron_1).to be_a Patron
   end
 
-  xit 'should inform the instance name' do
+  it 'should inform the instance name' do
     expect(patron_1.name).to eq "Bob"
   end
 
-  xit 'should inform how much money instance has' do
+  it 'should inform how much money instance has' do
     expect(patron_1.spending_money).to eq 20
   end
 
-  xit 'should inform what interest the instance' do
+  it 'should inform what interest the instance' do
     patron_1.interests
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
